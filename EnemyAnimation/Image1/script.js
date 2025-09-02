8e3bedfc62116419b0e8fef04851e098509c8140
Harhwardhan-1306 <harshwardhandeore.8975@gmail.com>
2025-09-02 13:02:35 +0530

    Completed Image1

--- EnemyAnimation/Image1/script.js
+++ EnemyAnimation/Image1/script.js
@@ -5,36 +5,36 @@ const ctx = canvas.getContext("2d")
 
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+const numberOfEnemies = 10;
 const enemyArray = [];
 let gameFrame = 0;
 
-const enemyImage = new Image();
-enemyImage.src = "./public/enemy1.png";
-
 class Enemy {
     constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.enemyImage = new Image();
+        this.enemyImage.src = "./public/enemy1.png";
 
-        this.speed = (Math.random() * 4) - 2;
+        //this.speed = (Math.random() * 4) - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
-        this.frame = 0
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1) 
     }
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
         //animate sprite
-        if(gameFrame % 3 === 0) {
+        if(gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }
     }
     draw() {
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
